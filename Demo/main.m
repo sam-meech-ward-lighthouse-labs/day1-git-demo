25b74791d100a333d0f7f7b5918f854dbf4d07d3
Sam Meech-Ward <sam@meech-ward.com>
2017-10-02 12:33:04 -0700

    Initial Commit

---
+++ Demo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Demo
+//
+//  Created by Sam Meech-Ward on 2017-10-02.
+//  Copyright © 2017 Sam Meech-Ward. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
